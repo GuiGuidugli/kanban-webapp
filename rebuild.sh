6565717603a9f47edb96859d203e7e3515c931b0
Guilherme Guidugli <guilhermeguidugli@pm.me>
2026-01-10 13:56:23 -0300

    Added bash aliases file, replacing the .sh script

--- rebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker rm -f webapp-dev
-docker build -t webapp .
-docker run -d -p 8080:8080 -e ASPNETCORE_ENVIRONMENT=Development --name webapp-dev webapp
-echo "Container rebuilt and started."
